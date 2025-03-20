8139c336d629808c607761941b62aacb46bf87d8
Nea4321 <ycs2219@naver.com>
2025-03-21 04:46:26 +0900

    Spring 개발환경 구성

---
+++ src/test/java/kr/yuhancert/spring/YcsCertificateSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.yuhancert.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YcsCertificateSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
