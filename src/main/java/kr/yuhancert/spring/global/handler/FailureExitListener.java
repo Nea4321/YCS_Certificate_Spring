e04b01a66b5006d8cc23fd0a6c8884a8fc01e3fa
Nea4321 <ycs2219@naver.com>
2025-10-27 02:49:59 +0900

    [ + ] 맥클에만 에러전송

--- src/main/java/kr/yuhancert/spring/global/handler/FailureExitListener.java
+++ src/main/java/kr/yuhancert/spring/global/handler/FailureExitListener.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringApplicationRunListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.core.NestedExceptionUtils;
 
 public class FailureExitListener implements SpringApplicationRunListener {
 
@@ -13,16 +14,25 @@ public class FailureExitListener implements SpringApplicationRunListener {
 
     @Override
     public void failed(ConfigurableApplicationContext context, Throwable exception) {
-        System.err.println("❌ Spring Boot Application Failed to Start or Run! ❌");
         Dotenv dotenv = Dotenv.load();
         String webhookUrl = dotenv.get("DISCORD_WEBHOOK"); // 환경변수로 읽기
+
         if (webhookUrl == null) {
             System.err.println("❌ DISCORD_WEBHOOK 환경변수가 설정되지 않음");
             return;
         }
-        DiscordNotifier notifier = new DiscordNotifier(webhookUrl);
-        notifier.sendMessage("🚨 **스프링 부팅 실패 발생!**\n" +
-                "주인님, 빨리 오셔야겠는데요..?");
+
+        Throwable rootCause = NestedExceptionUtils.getMostSpecificCause(exception);
+        String message = rootCause != null ? rootCause.getMessage() : null;
+
+        if (message != null && (message.contains("Max client") || message.contains("Too many connections"))) {
+
+            // Max Client 에러가 발생했을 때 실행할 코드
+            System.err.println("🚨 Max Client Exceeded Error Detected! Initiating specific cleanup...");
+            DiscordNotifier notifier = new DiscordNotifier(webhookUrl);
+            notifier.sendMessage("🚨 **DB 연결 실패 발생!**\n" +
+                    "주인님, 빨리 오셔야겠는데요..?");
+        }
     }
 
 }
